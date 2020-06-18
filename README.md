5a9be20cf0f9a05cdc0bb0f35a47ef3ce12bd45a
Jordan Pommerenck <43046344+Jordan-Pommerenck@users.noreply.github.com>
2020-06-18 14:17:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TheLastOpenGalaxy
+
+Developed with Unreal Engine 4
---
+++ Source/TheLastOpenGalaxy/TheLastOpenGalaxyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheLastOpenGalaxyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELASTOPENGALAXY_API ATheLastOpenGalaxyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
