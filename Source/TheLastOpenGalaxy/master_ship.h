6b23c1244abd5861e2b9f0d45e18802fc940deeb
Jordan Pommerenck <43046344+Jordan-Pommerenck@users.noreply.github.com>
2020-06-22 16:03:23 -0700

    Added the C++ master class

---
+++ Source/TheLastOpenGalaxy/master_ship.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "master_ship.generated.h"
+
+UCLASS()
+class THELASTOPENGALAXY_API Amaster_ship : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	Amaster_ship();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float base_speed;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float current_speed;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float base_acceleration;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float current_acceleration;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float base_pitch;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float base_yaw;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float base_roll;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float current_pitch;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float current_yaw;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Movement")
+		float current_roll;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
